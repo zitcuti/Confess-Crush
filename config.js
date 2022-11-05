c5060792bc7e2435f291d8c36507fc2951b164a9
zitcuti <117514788+zitcuti@users.noreply.github.com>
2022-11-05 17:47:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Khảo sát lần một",
+    introTitle: 'NAH',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
